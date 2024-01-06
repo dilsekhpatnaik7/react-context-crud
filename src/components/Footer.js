d77e22dbb3f1cd253a9888b42e9c6e4fd3cfd730
Dilsekh Patnaik <dpj020@nist.edu>
2024-01-06 13:59:01 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-primary py-12">
       <div className="container mx-auto">
         <p className="text-white text-center">
-          Copyright &copy; Neelam Padhy 🤍 2023. All rights reserved.
+          Maintained by Dilsekh Patnaik 🤍 2024.
         </p>
       </div>
     </footer>
